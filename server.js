06cedde72037cfd5f6c1d2c7dce9ee74762b3031
Akshay Poudel <akshaypoudel66@gmail.com>
2026-05-22 15:37:26 +0530

    changed code

--- server.js
+++ server.js
@@ -10,26 +10,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const db = mysql.createPool({
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
 
     if (err) {
-        console.log(err);
+        console.log("Database connection failed:", err);
         return;
     }
 
     console.log("MySQL Connected");
-});
 
+    connection.release();
+});
 app.post("/verify", (req, res) => {
 
     const { certificate_no, dob } = req.body;
+    console.log(req.body);
+    console.log(certificate_no);
+    console.log(dob);
 
     const query = `
 SELECT s.roll_number, s.full_name,s.father_name,s.gender,s.course_name,s.batch_shift,s.joining_date,s.student_status,c.certificate_number,
